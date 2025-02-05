c22368cd3392bf4ee8be12ca8016e896d9b0d2c6
abdulbori <abdulborimahamatjanov86@gmail.com>
2025-02-05 16:08:53 +0600

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
 import productRouter from './product.routes.js';
 import categoryRoute from './category.routes.js';
-<<<<<<< HEAD
 import orderRouter from './order.routes.js';
 import orderItemRouter from './orderItem.routes.js';
-
-mainRoute.use("/", productRouter);
-mainRoute.use("/", categoryRoute);
-mainRoute.use("/", orderRouter);
-mainRoute.use("/", orderItemRouter)
-=======
 import categoryItemRoute from './categoryItem.routes.js';
 import brandRoute from './brand.routes.js';
 import userRoute from './user.routes.js';
@@ -21,6 +14,7 @@ mainRoute.use("/",categoryRoute);
 mainRoute.use('/', categoryItemRoute);
 mainRoute.use('/', brandRoute);
 mainRoute.use('/', userRoute);
->>>>>>> 01120378928b738cc98abaffb8be7adb5191b40c
+mainRoute.use("/", orderRouter);
+mainRoute.use("/", orderItemRouter)
 
 export default mainRoute;
